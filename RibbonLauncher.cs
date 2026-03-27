87f295a6947a087345bed49c739a9b8c8196c78e
seamlee <lilymagnc@gmail.com>
2026-03-27 16:47:15 +0900

    feat(installer): remove batch installer, add auto-registry to launch_service.exe

--- RibbonLauncher.cs
+++ RibbonLauncher.cs
@@ -22,6 +22,17 @@ namespace ServiceLauncher
                     WorkingDirectory = appDir
                 };
 
+                // 자동 실행 레지스트리 자동 등록 (배치파일 필요 없도록)
+                try
+                {
+                    string exePath = Process.GetCurrentProcess().MainModule.FileName;
+                    using (Microsoft.Win32.RegistryKey key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true))
+                    {
+                        key.SetValue("RibbonBridgeService", "\"" + exePath + "\"");
+                    }
+                }
+                catch { }
+
                 // Kill existing process first for clean restart
                 foreach (var process in Process.GetProcessesByName("sys_service"))
                 {
